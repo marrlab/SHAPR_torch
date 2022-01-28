5c4b052a94b8762d8bba6aefd4e2a82202cde00d
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2022-01-28 18:03:29 +0100

    Some more fiddling with job submission script

--- shapr/topo_submit.sh
+++ shapr/topo_submit.sh
@@ -2,18 +2,9 @@
 
 if [[ "$#" -eq  "0" ]]; then
   echo "Submitting job with default values"
-  SCRIPT="poetry run python run_train_script.py"
+  sbatch -p interactive_gpu_p -J "topo_shapr" -o "topo_shapr%j.out" --gres=gpu:1 --qos=interactive_gpu --cpus-per-task=4 --mem=4G --nice=10000 --wrap "poetry run python run_train_script.py" 
  else
   echo "Submitting with parameters from $1"
-  SCRIPT="poetry run python run_train_script.py --params $1"
+  sbatch -p interactive_gpu_p -J "topo_shapr" -o "topo_shapr%j.out" --gres=gpu:1 --qos=interactive_gpu --cpus-per-task=4 --mem=4G --nice=10000 --wrap "poetry run python run_train_script.py --params $1"
 fi
 
-srun -p interactive_gpu_p  \ 
-     -J "topo_shapr"       \
-     -o "topo_shapr%j.out" \
-     --gres=gpu:1          \ 
-     --qos=interactive_gpu \
-     --cpus-per-task=4     \ 
-     --mem=4G              \
-     --nice=10000          \
-     --wrap ${SCRIPT}
