a5bfd21071764979af677104329e73d635fdeea9
Bastian Rieck <bastian@rieck.me>
2022-02-08 19:16:46 +0100

    Updated run scripts to support different scenarios

--- shapr/run_train_script.py
+++ shapr/run_train_script.py
@@ -1,9 +1,8 @@
-"""Run training with optional external parameters."""
+"""Run training with optional external parameters file."""
 
 import argparse
 
 from shapr import run_train
-from shapr._settings import SHAPRConfig
 
 
 if __name__ == '__main__':
@@ -15,27 +14,5 @@ if __name__ == '__main__':
         help='Config file'
     )
 
-    default_config = SHAPRConfig()
-
-    # Check which arguments we can actually override from outside.
-    shapr_args = set()
-
-    for k, _ in default_config._SHAPRConfig__config_param_names.items():
-        parser.add_argument(
-            f'--{k}',
-            type=type(default_config.__getattribute__(k)),
-        )
-
-        shapr_args.add(k)
-
     args = parser.parse_args()
-
-    # Prepare arguments that were overridden by the user from the
-    # command-line (or via a sweep, for instance).
-    overrides = {}
-
-    for k, v in vars(args).items():
-        if k in shapr_args and v is not None:
-            overrides[k] = v
-
-    run_train(params=args.params, overrides=overrides)
+    run_train(params=args.params)
---
+++ shapr/sweep.py
@@ -0,0 +1,34 @@
+"""Run hyperparameter sweep."""
+
+import argparse
+
+from shapr import run_train
+from shapr._settings import SHAPRConfig
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    default_config = SHAPRConfig()
+
+    # Check which arguments we can actually override from outside.
+    shapr_args = set()
+
+    for k, _ in default_config._SHAPRConfig__config_param_names.items():
+        parser.add_argument(
+            f'--{k}',
+            type=type(default_config.__getattribute__(k)),
+        )
+
+        shapr_args.add(k)
+
+    args = parser.parse_args()
+
+    # Prepare arguments that were overridden by the user from the
+    # command-line (or via a sweep, for instance).
+    overrides = {}
+
+    for k, v in vars(args).items():
+        if k in shapr_args and v is not None:
+            overrides[k] = v
+
+    run_train(params=args.params, overrides=overrides)
