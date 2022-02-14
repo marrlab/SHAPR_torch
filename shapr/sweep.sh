b4910af5b20f38d742f0880b5332b4f4d46f5422
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2022-02-14 10:50:09 +0100

    Updated sweep configuration

--- shapr/sweep.sh
+++ shapr/sweep.sh
@@ -9,10 +9,11 @@ if [ -z "$1" ]; then
   exit -1
 fi
 
-for i in `seq 27`; do
+for i in `seq 90`; do
   sbatch -p gpu_p               \
          -J ${NAME}             \
          -o "${NAME}_%j.out"    \
+         -x supergpu[05-08]     \
          --gres=gpu:1           \
          --qos=gpu              \
          --cpus-per-task=4      \
