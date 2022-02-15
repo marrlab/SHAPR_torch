d4b54d9b5e6c1c6b6a5abc868fb467cfd59d866f
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2022-02-15 14:41:22 +0100

    Made number of jobs configurable for sweep submission script

--- shapr/sweep.sh
+++ shapr/sweep.sh
@@ -9,7 +9,13 @@ if [ -z "$1" ]; then
   exit -1
 fi
 
-for i in `seq 90`; do
+if [ -z "$2" ]; then
+  N_JOBS=48;
+else
+  N_JOBS=$2;
+fi
+
+for i in `seq ${N_JOBS}`; do
   sbatch -p gpu_p               \
          -J ${NAME}             \
          -o "${NAME}_%j.out"    \
