fff5eb0ef9bda43fb74036136d71d494710bf580
Bastian Rieck <bastian@rieck.me>
2022-02-16 08:34:01 +0100

    Added restricted scenario

--- shapr/analyse_wandb.py
+++ shapr/analyse_wandb.py
@@ -49,6 +49,16 @@ def show_best_performance(df):
         df_grouped[means].mean(axis=1).idxmin(axis=0)
     )
 
+    print('\nRestricting to scenarios with small regularisation strength:\n')
+
+    # Filter scenario that we converged on.
+    df = df.query('topo_lambda == 0.1 and topo_interp < 24')
+
+    topo_params.remove('topo_lambda')
+    df = df.drop('topo_lambda', axis='columns')
+
+    df_grouped = df.groupby(topo_params).agg(agg)
+    print(df_grouped)
 
 
 if __name__ == '__main__':
