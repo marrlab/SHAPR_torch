37e6fcc2a62729412f35b28cd5eb4c1d56a34bcb
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2022-02-18 14:17:44 +0100

    Renamed run script

--- shapr/run.sh
+++ shapr/run.sh
@@ -5,6 +5,6 @@ if [[ "$#" -eq  "0" ]]; then
   sbatch -p interactive_gpu_p -J "topo_shapr" -o "topo_shapr%j.out" --gres=gpu:1 --qos=interactive_gpu --cpus-per-task=4 --mem=4G --nice=10000 --wrap "poetry run python run_train_script.py" 
  else
   echo "Submitting with parameters from $1"
-  sbatch -p interactive_gpu_p -J "topo_shapr" -o "topo_shapr%j.out" --gres=gpu:1 --qos=interactive_gpu --cpus-per-task=4 --mem=4G --nice=10000 --wrap "poetry run python run_train_script.py --params $1"
+  sbatch -p gpu_p -J "topo_shapr" -o "topo_shapr%j.out" --gres=gpu:1 --qos=gpu --cpus-per-task=4 --mem=4G --nice=10000 --wrap "poetry run python run_train_script.py --params $1"
 fi
 
