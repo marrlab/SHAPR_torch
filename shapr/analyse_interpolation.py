6e5a18c997b9f2989dfad3b4ea5a215d6a71d531
Bastian Rieck <bastian@rieck.me>
2022-02-22 09:47:36 +0100

    Skeleton for interpolation analysis script

---
+++ shapr/analyse_interpolation.py
@@ -0,0 +1,44 @@
+"""Analyse interpolation errors of input data set."""
+
+import argparse
+import os
+import torch
+
+from shapr._settings import SHAPRConfig
+from shapr.data_generator import SHAPRDataset
+
+from torch.utils.data import DataLoader
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p', '--params',
+        type=str,
+        default=None,
+        help='Config file'
+    )
+
+    args = parser.parse_args()
+
+    settings = SHAPRConfig(params=args.params)
+
+    all_files = os.listdir(
+        os.path.join(settings.path, "obj/")
+    )
+
+    data_set = SHAPRDataset(settings.path, all_files, settings.random_seed)
+    loader = DataLoader(data_set, batch_size=8, shuffle=True)
+
+    all_dfs = []
+    index = 0
+
+    for _, objects in loader:
+        size = settings.topo_interp
+        objects_interp = torch.nn.functional.interpolate(
+            input=objects, size=(size, ) * 3,
+        )
+
+        objects_recon = torch.nn.functional.interpolate(
+            input=objects_interp, size=(64, ) * 3,
+        )
