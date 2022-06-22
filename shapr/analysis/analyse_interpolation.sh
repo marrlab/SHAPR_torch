c1a4fcaece615b954a5707b68b88c14cfe3509a4
Bastian Rieck <bastian@rieck.me>
2022-06-22 09:43:04 +0200

    Adjusted path for interpolation analysis

--- shapr/analysis/analyse_interpolation.sh
+++ shapr/analysis/analyse_interpolation.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 for i in `seq 64`; do
-  python analyse_interpolation.py -s $i -p config/small-0D.json
+  python analyse_interpolation.py -s $i -p ../config/small-0D.json
 done
 
